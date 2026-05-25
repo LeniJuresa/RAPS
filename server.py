486df11cbb9349eebdb4ca1a11ebffa46a300295
leni <leni.juresa@gmail.com>
2026-05-25 18:28:54 +0200

    doradio duljinu brojeva za latitudu i longitudu

--- server.py
+++ server.py
@@ -8,8 +8,8 @@ ISS_API = 'https://api.wheretheiss.at/v1/satellites/25544'
 def get_iss_data():
     data = requests.get(ISS_API).json()
     return {
-        'lat': round(data['latitude'], 2),
-        'lon': round(data['longitude'], 2),
+        'lat': round(data['latitude'], 4),
+        'lon': round(data['longitude'], 4),
         'alt': round(data['altitude'], 0),
         'speed': round(data['velocity'])
     }
