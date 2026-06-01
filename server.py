1a050ef8491d3eede3c3da723e5fbb8bd89296f7
leni <leni.juresa@gmail.com>
2026-06-01 12:27:46 +0200

    priprema za render hosting

--- server.py
+++ server.py
@@ -24,5 +24,8 @@ def index():
 def iss():
     return jsonify(get_iss_data())
 
-if __name__ == '__main__':
-    app.run(debug=True)
+import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
